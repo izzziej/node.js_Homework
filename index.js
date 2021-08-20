fc342e2de66095a92ae367dfe49b9be4680189b0
Isabelle Jacobsen <84287535+izzziej@users.noreply.github.com>
2021-08-20 16:23:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,86 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+function writeUp(fileBody){
+    fs.writeFile(" readme.txt ", fileBody , (err) => { 
+        err? console.log (err) : console.log ("Write Up Complete")})
+}
+
+;
 inquirer.prompt ([
     {
       type: 'input',
       name: 'title',
-      message: 'Title of Repo',
+      message: 'Title of Repo:',
     },
     {
       type: 'input',
-      message: 'Description',
+      message: 'Description:',
       name: 'description',
     },
     {
       type: 'input',
-      message: 'Installation Instructions',
+      message: 'Installation Instructions:',
       name: 'install',
     },
     {
       type: 'input',
-      name: 'Usage',
-      message: 'Usage Info',
+      name: 'usage',
+      message: 'Usage Info:',
     },
     {
       type: 'input',
       name: 'contribution',
-      message: 'Contribution Info',
+      message: 'Contribution Info:',
     },
     {
       type: 'input',
       name: 'test',
-      message: 'Test Instructions',
+      message: 'Test Instructions:',
       },
       {
         type: 'list',
-        message: 'What is your preferred method of communication?',
+        message: 'Licence:',
         name: 'license',
         choices: ['MIT', 'GNU', 'NONE' ],
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Github Page',
+        message: 'Github Page:',
       },
       {
         type: 'input',
         name: 'email',
-        message: 'Email Address ',
+        message: 'Email Address: ',
       },
-      
-  ]) 
 
-  ;k
- 
+])
+  .then ((responseObj) => {
+    console.log(responseObj);
+    let fileBody = `
+    ${responseObj.title} 
+    "Description: " ${responseObj.description}
+
+    "Installation Instructions: "${responseObj.install} 
+
+    "License: " ${responseObj.license}
+
+    "Usage: " ${responseObj.usage}
+
+    "Contribution: "${responseObj.contribution} 
+
+    "Test Instructions: " ${responseObj.test}
+
+    "Git Hub Page:" ${responseObj.github} 
+    
+    "Email Me At: "${responseObj.email}
+    `; 
+    
+    writeUp(fileBody);
+
+    });
+
+
+   
+  
